5801c434f995b28d607640a29e6c02691c942a2a
Rumman <md.mazidulhasan1@gmail.com>
2023-05-01 20:53:19 +0600

    Advanced Reccursion by permutation

---
+++ src/main/java/com/Recursion/Intermediate/permutation.java
@@ -0,0 +1,21 @@
+package com.Recursion.Intermediate;
+
+public class permutation {
+    public static void printPerm(String str, String permutation){
+        if (str.length() == 0) {
+            System.out.println(permutation);
+            return;
+        }
+
+        for (int i = 0; i < str.length(); i++) {
+            char currentChar = str.charAt(i);
+            String newString = str.substring(0, i) + str.substring(i+1);
+            printPerm(newString, permutation+currentChar);
+        }
+    }
+
+    public static void main(String[] args) {
+        String str = "abc";
+        printPerm(str, "");
+    }
+}
