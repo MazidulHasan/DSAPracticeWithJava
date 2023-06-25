731bfedd35c3defd1479fee492de7414f184e60a
Rumman <md.mazidulhasan1@gmail.com>
2023-06-25 23:48:48 +0600

    Cutting rod

---
+++ src/main/java/com/DynamicProgramming/CuttingARod.java
@@ -0,0 +1,14 @@
+package com.DynamicProgramming;
+
+public class CuttingARod {
+    
+    public int cutRod(int price[], int n){
+        int dp[] = new int[n+1];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                dp[i] = Math.max(dp[i], price[j]+dp[i-j-1]);
+            }
+        }
+        return dp[n];
+    }
+}
