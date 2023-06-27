f6ae2d846b8e080c49a8212002dece652406aabd
MazidulHasan <39567554+MazidulHasan@users.noreply.github.com>
2023-06-27 22:19:39 +0600

    leetCode countNegatives

---
+++ src/main/java/com/LeetCodePractise/countNegatives.java
@@ -0,0 +1,26 @@
+package temp;
+
+public class tempPractise1 {
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {4,3,2,-1},
+            {3,2,1,-1},
+            {1,1,-1,-2},
+            {-1,-1,-2,-3}
+        };
+        System.out.println("The count is : "+countNegatives(matrix));
+    }
+
+    public static int countNegatives(int[][] grid) {
+        int count = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] < 0) {
+                    System.out.println(grid[i][j]);
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
