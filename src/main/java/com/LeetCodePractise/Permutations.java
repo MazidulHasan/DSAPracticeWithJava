9c5845a07c3f4e8573c375a43761d7dce51eb183
MazidulHasan <md.mazidulhasan1@gmail.com>
2023-08-17 22:56:23 +0600

    LeetCode Permutation

---
+++ src/main/java/com/LeetCodePractise/Permutations.java
@@ -0,0 +1,29 @@
+package com.LeetCodePractise;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Permutations {
+    
+    public List<List<Integer>> permute(int[] nums) {
+        LinkedList<List<Integer>> result = new LinkedList<List<Integer>>();
+        int rSize;
+        result.add(new ArrayList<Integer>());
+
+        for (int num : nums) {
+            rSize = result.size();
+
+            while (rSize>0) {
+                List<Integer> permutation = result.pollFirst();
+                for (int i = 0; i < permutation.size(); i++) {
+                    List<Integer> newPermutation = new ArrayList<Integer>(permutation);
+                    newPermutation.add(i, num);
+                    result.add(newPermutation);
+                }
+                rSize --;
+            }
+        }
+        return result;
+    }
+}
