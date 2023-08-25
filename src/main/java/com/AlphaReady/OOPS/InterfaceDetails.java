21436760e0568d7cadf470e98202247dfdb7efd9
MazidulHasan <md.mazidulhasan1@gmail.com>
2023-08-25 23:34:11 +0600

    Interface Basic

---
+++ src/main/java/com/AlphaReady/OOPS/InterfaceDetails.java
@@ -0,0 +1,28 @@
+package com.AlphaReady.OOPS;
+
+public class InterfaceDetails {
+    public static void main(String[] args) {
+        // Queen q = new Queen();   
+    }
+    interface ChecssPlayer{
+        void moves(); //by default public and abstract
+    }
+
+    class Queen implements ChecssPlayer{
+        public void moves(){
+            System.out.println("up down left right diagonal");
+        }
+    }
+
+    class Rook implements ChecssPlayer{
+        public void moves(){
+            System.out.println("up down left right");
+        }
+    }
+
+    class King implements ChecssPlayer{
+        public void moves(){
+            System.out.println("up down left right diagonal by one step");
+        }
+    }
+}
