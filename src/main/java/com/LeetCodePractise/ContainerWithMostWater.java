cb26edecb7a9c0d4524e71217805cf99a497a624
MazidulHasan <39567554+MazidulHasan@users.noreply.github.com>
2023-09-22 22:40:20 +0600

    LeetCode Rain water problem sol with two pointers

---
+++ src/main/java/com/LeetCodePractise/ContainerWithMostWater.java
@@ -0,0 +1,30 @@
+package temp;
+
+public class ContainerWithMostWater {
+    public static int maxArea(int[] height) {
+        int maxSum = 1;
+        int pointer1 = height[0];
+        for (int i = 0; i < height.length - 1; i++) {
+            System.out.println("Max Sum :"+maxSum);
+            if (pointer1 < height[i]) {
+                pointer1 = height[i];
+            }
+            int count = 1;
+            for (int j = i+1; j < height.length; j++) {
+                if (height[i] >= height[j]) {
+                    int currnetHeight = count*height[j];
+                    System.out.println("Current Height"+count+"*"+height[j]+"="+currnetHeight);
+                    if (maxSum < currnetHeight) {
+                        maxSum = currnetHeight;
+                    }
+                }
+                count++;
+            }
+        }
+        return maxSum;
+    }
+    public static void main(String[] args) {
+        int [] height = {1,8,6,2,5,4,8,3,8};
+        System.out.println(maxArea(height));
+    }
+}
